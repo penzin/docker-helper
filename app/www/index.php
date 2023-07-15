14902cab316e81fa5f5345e073054800a7584c37
Roman Penzin <penzin.r.v@gmail.com>
2023-07-15 12:55:04 +0300

    Update index.php, remove unnecessary code

--- app/www/index.php
+++ app/www/index.php
@@ -1,14 +1,3 @@
 <?php
 
-    error_reporting(E_ALL);
-    ini_set("display_errors", "on");
-    
-    echo "<h1>Docker-compose: Nginx + php-fpm7.1 + MySQL8 test App</h1>";
-    
-    //host = db, see docker-compose.yml
-    $pdo = new PDO("mysql:host=db;charset=utf8;dbname=mysql", "root", "root");
-    $users = $pdo->query("SELECT * FROM user")->fetchAll();
-    
-    echo "<pre>";
-    print_r($users);
-    echo "</pre>";
+echo "Docker-compose: Nginx + php-fpm8.2 + MySQL8 test App";
