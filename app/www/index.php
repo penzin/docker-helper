91d2d882c57233e2515ea9b3f01c07601cc9f8aa
Roman Penzin <penzin.r.v@gmail.com>
2023-07-15 14:36:46 +0300

    Actualize index.php to display actual versions of PHP and MySQL

--- app/www/index.php
+++ app/www/index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "Docker-compose: Nginx + php-fpm8.2 + MySQL8 test App";
+echo 'Project run under docker-compose: Nginx latest + php-fpm8.2 + MySQL8<br />';
+echo 'PHP Version: ' . PHP_VERSION . '<br />';
+
+$connection = new PDO('mysql:host=db;charset=utf8;dbname=mysql', 'root', 'root');
+$connection->query('SELECT version();')->fetchColumn(0);
+
+echo 'Mysql version: ' . $connection->query('SELECT version();')->fetchColumn(0);
